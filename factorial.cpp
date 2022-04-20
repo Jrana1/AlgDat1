201cc7dc208a4fdc7da1806b6f75d6fcda8bbeb6
Jrana1 <jewelbutex12@gmail.com>
2022-04-20 22:21:35 +0200

    factorial iterative added

--- factorial.cpp
+++ factorial.cpp
@@ -8,11 +8,22 @@ int factorial_recursive(int n)
         return 1;
     return n * factorial_recursive(n - 1);
 }
+int factorial_iterative(int n)
+{
+
+    int ans = 1;
+    while (n > 1)
+    {
+        ans *= n;
+        n--;
+    }
+    return ans;
+}
 int main()
 {
 
     int n = 4;
-    cout << factorial_recursive(n) << endl;
+    cout << factorial_iterative(n) << endl;
 
     return 0;
 }
